9f379c31954056fc4d180622e9fb31b46ed2a000
lonelyuan <139369370@qq.com>
2021-12-08 01:00:52 +0800

    finish rdt_v20; add javadoc

--- src/com/ouc/tcp/test/CheckSum.java
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -7,8 +7,11 @@ import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 
 public class CheckSum {
-
-    /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+    /**
+     * 计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段
+     * @param tcpPack 要计算的包
+     * @return crc校验和
+     */
     public static short computeChkSum(TCP_PACKET tcpPack) {
         CRC32 crc = new CRC32();
         crc.update(tcpPack.getTcpH().getTh_seq());
--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -9,51 +9,49 @@ import java.io.IOException;
 
 import com.ouc.tcp.client.TCP_Receiver_ADT;
 import com.ouc.tcp.message.*;
-import com.ouc.tcp.tool.TCP_TOOL;
 
 public class TCP_Receiver extends TCP_Receiver_ADT {
-	
-	private TCP_PACKET ackPack;	//回复的ACK报文段
-	int sequence=1;//用于记录当前待接收的包序号，注意包序号不完全是
-		
-	/*构造函数*/
+
+	/**
+	 * 序列号
+	 */
+	private int seq;
+
 	public TCP_Receiver() {
-		super();	//调用超类构造函数
-		super.initTCP_Receiver(this);	//初始化TCP接收端
+		super();
+		super.initTCP_Receiver(this);
 	}
 
+	/**
+	 * 检查校验码，生成ACK
+	 * @param recvPack 收到的包
+	 */
 	@Override
-	//接收到数据报：检查校验和，设置回复的ACK报文段
 	public void rdt_recv(TCP_PACKET recvPack) {
-		//检查校验码，生成ACK
+		TCP_PACKET ackPack; //回复的ACK报文段
 		if(CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
-			//生成ACK报文段（设置确认号）
-			tcpH.setTh_ack(recvPack.getTcpH().getTh_seq());
+			tcpH.setTh_ack(recvPack.getTcpH().getTh_seq()); //ACK
 			ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
-			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
-			//回复ACK报文段
-			reply(ackPack);			
-			
+			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack)); //回复ACK报文段
+			reply(ackPack);
 			//将接收到的正确有序的数据插入data队列，准备交付
-			dataQueue.add(recvPack.getTcpS().getData());				
-			sequence++;
+  			//System.out.println("[*] seq now:" + sequence);
+ 			//System.out.println("[*] seq recv:" + recvPack.getTcpH().getTh_seq());
+			if(recvPack.getTcpH().getTh_seq() != seq){
+				dataQueue.add(recvPack.getTcpS().getData());
+				seq = recvPack.getTcpH().getTh_seq();
+			}else {
+				System.out.println("[!] seq repeated!");
+			}
 		}else{
-			System.out.println("Recieve Computed: "+CheckSum.computeChkSum(recvPack));
-			System.out.println("Recieved Packet"+recvPack.getTcpH().getTh_sum());
-			System.out.println("Problem: Packet Number: "+recvPack.getTcpH().getTh_seq()+" + InnerSeq:  "+sequence);
-			tcpH.setTh_ack(-1);
+			System.out.println("[!] check sum failed!");
+			tcpH.setTh_ack(-1); // NAK
 			ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
 			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
-			//回复ACK报文段
 			reply(ackPack);
 		}
-		
-		System.out.println();
-		
-		
 		//交付数据（每20组数据交付一次）
-		if(dataQueue.size() == 20) 
-			deliver_data();	
+		if(dataQueue.size() == 20) deliver_data();
 	}
 
 	@Override
@@ -62,35 +60,31 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 		//检查dataQueue，将数据写入文件
 		File fw = new File("recvData.txt");
 		BufferedWriter writer;
-		
 		try {
 			writer = new BufferedWriter(new FileWriter(fw, true));
-			
 			//循环检查data队列中是否有新交付数据
 			while(!dataQueue.isEmpty()) {
 				int[] data = dataQueue.poll();
-				
 				//将数据写入文件
 				for(int i = 0; i < data.length; i++) {
 					writer.write(data[i] + "\n");
 				}
-				
 				writer.flush();		//清空输出缓存
 			}
 			writer.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * 回复ACK报文段
+	 * @param replyPack 返回包
+	 */
 	@Override
-	//回复ACK报文段
 	public void reply(TCP_PACKET replyPack) {
 		//设置错误控制标志
-		tcpH.setTh_eflag((byte)0);	//eFlag=0，信道无错误
-				
-		//发送数据报
+		tcpH.setTh_eflag((byte)1);
 		client.send(replyPack);
 	}
 	
