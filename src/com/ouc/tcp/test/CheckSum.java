467d439b065036cd10d65fcb6ac75a64a395a612
lonelyuan <139369370@qq.com>
2021-12-07 16:41:35 +0800

    add CheckSum.java

--- src/com/ouc/tcp/test/CheckSum.java
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -7,10 +7,18 @@ import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 
 public class CheckSum {
-	
-	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
-	public static short computeChkSum(TCP_PACKET tcpPack) {
-		int checkSum = 0;
-		return (short) checkSum;
-	}
+
+    /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+    public static short computeChkSum(TCP_PACKET tcpPack) {
+        CRC32 crc = new CRC32();
+        crc.update(tcpPack.getTcpH().getTh_seq());
+        crc.update(tcpPack.getTcpH().getTh_ack());
+        int[] data = tcpPack.getTcpS().getData();
+        for (int datum : data) {
+            crc.update(datum);
+        }
+        crc.update(Arrays.toString(tcpPack.getTcpS().getData()).getBytes());
+        //System.out.println(checkSum);
+        return (short) crc.getValue();
+    }
 }
