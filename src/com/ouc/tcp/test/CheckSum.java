0fe295488235a35a228d10797319dd6e3cd2aff0
lonelyuan <139369370@qq.com>
2021-12-07 16:04:04 +0800

    init project

--- src/com/ouc/tcp/test/CheckSum.java
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -1,5 +1,6 @@
 package com.ouc.tcp.test;
 
+import java.util.Arrays;
 import java.util.zip.CRC32;
 
 import com.ouc.tcp.message.TCP_HEADER;
@@ -10,10 +11,6 @@ public class CheckSum {
 	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
 	public static short computeChkSum(TCP_PACKET tcpPack) {
 		int checkSum = 0;
-		
-		
-		
 		return (short) checkSum;
 	}
-	
 }
--- src/com/ouc/tcp/test/TCP_Sender.java
+++ src/com/ouc/tcp/test/TCP_Sender.java
@@ -23,24 +23,23 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	@Override
 	//可靠发送（应用层调用）：封装应用层数据，产生TCP数据报；需要修改
 	public void rdt_send(int dataIndex, int[] appData) {
-		
 		//生成TCP数据报（设置序号和数据字段/校验和),注意打包的顺序
 		tcpH.setTh_seq(dataIndex * appData.length + 1);//包序号设置为字节流号：
 		tcpS.setData(appData);
-		tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);		
-		//更新带有checksum的TCP 报文头		
+		tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);
+		//更新带有checksum的TCP 报文头
 		tcpH.setTh_sum(CheckSum.computeChkSum(tcpPack));
 		tcpPack.setTcpH(tcpH);
-		
+
 		//发送TCP数据报
 		udt_send(tcpPack);
 		flag = 0;
-		
+
 		//等待ACK报文
 		//waitACK();
 		while (flag==0);
 	}
-	
+
 	@Override
 	//不可靠发送：将打包好的TCP数据报通过不可靠传输信道发送；仅需修改错误标志
 	public void udt_send(TCP_PACKET stcpPack) {
